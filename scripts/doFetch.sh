a75b678749314cd6bcf620826d0e0fa0e7d6ebf3
Mike Lloyd <mlloyd@pivotal.io>
2017-09-12 08:52:20 -0600

    fixed deadlock issue.
    Signed-off-by: Mike Lloyd <mlloyd@pivotal.io>

--- scripts/doFetch.sh
+++ scripts/doFetch.sh
@@ -5,7 +5,6 @@ unset PYTHONPATH
 
 echo "
 from azure.storage.blob import BlockBlobService
-import sys, io
 
 accessKey = 'f9T0B+jshYHi3UkfiuXsMOaQr9u9/YHHdwOGZg+MHhcLU8E3ANlJvGzf5ssxK//iWaOynMc7EPK151evwO9B5g=='
 containerName = 'gaia-data'
@@ -14,13 +13,14 @@ accountName = 'gaiadiag597'
 def download(segment):
     block_blob_service = BlockBlobService(account_name=accountName, account_key=accessKey)
 
+    def grouper(n, l):
+        return [l[i:i + n] for i in range(0, len(l), n)]
+
     generator = block_blob_service.list_blobs(containerName)
     blobs = [blob.name for blob in generator if blob.name.upper() not in ['MD5SUM', 'CITATION', 'DISCLAIMER']]
+    groups = grouper(16, blobs)
 
-    for idx, blobby in enumerate(blobs):
-        if len(blobby) == 0:
-            continue
-        if (idx % (segment + 1) ) == 0:
-            block_blob_service.get_blob_to_path(containerName, blobby, '/dev/stdout', max_connections=1)
+    for blobby in groups[segment]:
+        block_blob_service.get_blob_to_path(containerName, blobby, '/dev/stdout', max_connections=1)
 
 download(${GP_SEGMENT_ID})" | /home/gpadmin/anaconda3/bin/python -u | egrep -v "solution_id\,source_id"
